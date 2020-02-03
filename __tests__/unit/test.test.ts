4d672eb4670035d4fa628d7b63d35c60d8d289cf
Kristjan Kosic - Chris <chris@ark.io>
2020-02-03 13:49:54 +0100

    test: changed output of transacton build

--- __tests__/unit/test.test.ts
+++ __tests__/unit/test.test.ts
@@ -6,17 +6,17 @@ import { CertifiedDataTransaction } from "../../src/transactions";
 
 describe("Test builder", () => {
     it("Should verify correctly", () => {
-        Managers.configManager.setFromPreset("testnet");
+        Managers.configManager.setFromPreset("devnet");
         Managers.configManager.setHeight(2); // v2 transactions (aip11) are available from height 2
         Transactions.TransactionRegistry.registerTransactionType(CertifiedDataTransaction);
 
         const builder = new CertifiedDataBuilder();
         const actual = builder
-            .certifiedDataAsset({ data: "certifieddata" })
+            .certifiedDataAsset({ data: "ARK Core Blockchain Integration MasterClass :D" })
             .nonce("1")
             .sign("awful brand vocal relief wish afford avocado lobster run today wagon faith");
 
-        console.log(actual.build().toJson());
+        console.log(JSON.stringify(actual.build().toJson(), null, " "));
         expect(actual.build().verified).toBeTrue();
         expect(actual.verify()).toBeTrue();
     });
